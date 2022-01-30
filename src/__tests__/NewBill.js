e6f78954d4389147623601c0d6efe1206fd2a8a9
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2022-01-30 13:42:49 +0100

    The file loader is fixed. allows only .jpeg, .jpg, .png. image is shown in modals, file name is visible in admin dashborad

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,16 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { fireEvent, screen } from "@testing-library/dom";
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then i should load a file with .jpg/.jpeg/.png extention", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      //to-do write assertion
     })
   })
 })
